5fdd49afc58f04905ae92d322300f7a799100bcd
Piotr Ostrowski <piotrostr@google.com>
2024-05-27 16:05:14 +0200

    squeeze var

--- hack/rollout.sh
+++ hack/rollout.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-TAG=git rev-parse --short HEAD
-
 docker compose pull
 
-docker stack deploy "trader_$TAG" \
+docker stack deploy "trader_$(git rev-parse --short HEAD)" \
   --compose-file docker-compose.yml
