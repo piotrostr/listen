f0d7fbc4f6ff39916669c376cc9f65b97736d56d
Piotr Ostrowski <piotrostr@google.com>
2024-06-07 00:15:01 +0200

    fallback to req, add updated pump url

--- src/buyer.rs
+++ src/buyer.rs
@@ -235,11 +235,14 @@ pub async fn listen_for_burn(
 
 pub async fn check_if_pump_fun(mint: &Pubkey) -> Result<bool, Box<dyn Error>> {
     // easier way
-    return Ok(mint.to_string().ends_with("pump"));
-    // let base = "https://client-api-2-74b1891ee9f9.herokuapp.com/coins/";
-    // let url = format!("{}{}", base, mint);
-    // let res = reqwest::get(&url).await?;
-    // Ok(res.status().is_success())
+    if mint.to_string().ends_with("pump") {
+        return Ok(true);
+    } else {
+        let base = "https://frontend-api.pump.fun/coins/";
+        let url = format!("{}{}", base, mint);
+        let res = reqwest::get(&url).await?;
+        Ok(res.status().is_success())
+    }
 }
 
 #[cfg(test)]
