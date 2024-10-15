69bec5f0acba053e1f6de4005ca32d8cff594d14
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-10-15 23:13:22 +0200

    api dont work :/

--- watcher/main.py
+++ watcher/main.py
@@ -1,6 +1,8 @@
 import logging
 
 from watcher.dataset import get_or_create_candles_df, get_or_create_tokens_invested_df
+from watcher.types import Chain, Timeframe
+from watcher.watcher import Watcher
 
 log_format = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=log_format)
@@ -12,4 +14,11 @@ def make_data():
 
 
 if __name__ == "__main__":
-    pass
+    import time
+
+    watcher = Watcher(Chain.SOL)
+    watcher.get_first_1k_candles(
+        "Fosp9yoXQBdx8YqyURZePYzgpCnxp9XsfnQq69DRvvU4",
+        last_active_timestamp=int(time.time()),
+        timeframe=Timeframe.FIFTEEN_MINUTES,
+    )
