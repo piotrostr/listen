aa4107e3f53c9e02c2ee93acd0fe0f7727e9ed46
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-10-15 23:12:41 +0200

    dupa dupa

---
+++ chart_explorer/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+    <iframe id="tradingview_73726" name="tradingview_73726" src="about:blank" data-widget-options="symbol=MEDUSA%2FSOL&amp;interval=1&amp;widgetbar=%7B%22details%22%3Afalse%2C%22watchlist%22%3Afalse%2C%22news%22%3Afalse%2C%22datawindow%22%3Afalse%2C%22watchlist_settings%22%3A%7B%22default_symbols%22%3A%5B%5D%7D%7D&amp;timeFrames=%5B%7B%22text%22%3A%225y%22%2C%22resolution%22%3A%221W%22%7D%2C%7B%22text%22%3A%221y%22%2C%22resolution%22%3A%221W%22%7D%2C%7B%22text%22%3A%226m%22%2C%22resolution%22%3A%22120%22%7D%2C%7B%22text%22%3A%223m%22%2C%22resolution%22%3A%2260%22%7D%2C%7B%22text%22%3A%221m%22%2C%22resolution%22%3A%2230%22%7D%2C%7B%22text%22%3A%225d%22%2C%22resolution%22%3A%225%22%7D%2C%7B%22text%22%3A%221d%22%2C%22resolution%22%3A%221%22%7D%5D&amp;locale=en&amp;uid=tradingview_73726&amp;clientId=0&amp;userId=0&amp;chartsStorageVer=1.0&amp;autoSaveDelay=2&amp;debug=false&amp;timezone=Europe%2FWarsaw&amp;ssreqdelay=500&amp;theme=dark" title="Financial Chart" frameborder="0" allowtransparency="true" scrolling="no" allowfullscreen="" style="display: block; width: 100%; height: 100%;">
+	</iframe>
+	</div>
+  )
+}
+
+export default App
