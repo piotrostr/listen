679a9ebfcfe5279644f67d2e47afbd974fcc7442
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-01-16 23:34:15 +0100

    clean up

--- pump-ts/consts.ts
+++
@@ -1,3 +0,0 @@
-export const COIN_DATA_URL = "https://frontend-api.pump.fun/coins/";
-export const PUMP_WEBSOCKET_URL =
-  "wss://frontend-api.pump.fun/socket.io/?EIO=4&transport=websocket";
--- pump-ts/token.ts
+++
@@ -1,7 +0,0 @@
-import { COIN_DATA_URL } from "./consts";
-
-export async function grabToken(mint: string) {
-  const res = await fetch(COIN_DATA_URL + mint);
-  const json = await res.json();
-  return json;
-}
