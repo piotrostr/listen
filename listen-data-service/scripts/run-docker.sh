4e294ee8108e16d870ed9542d3b1f3f6812abe5b
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-02-11 14:04:25 +0100

    nits

--- listen-data-service/scripts/run-docker.sh
+++ listen-data-service/scripts/run-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TAG=$(git rev-parse --short HEAD)
-IMAGE=$(docker images -q listen-data-service:"$TAG")
+IMAGE="listen-data-service:$TAG"
 
 docker build -t "$IMAGE" . \
     && docker run -it --rm -v "$(pwd)"/.env:/app/.env "$IMAGE"
