aff2d037bac1afb0088b350886db70ed1be75252
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-02-11 16:21:45 +0100

    pack up preview

--- listen-data-service/preview/next.config.ts
+++ listen-data-service/preview/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"],
+    },
+  },
 };
 
 export default nextConfig;
