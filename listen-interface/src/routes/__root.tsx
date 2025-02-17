2a353cfa64d64b31dfa01940395137c73ff951b4
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-02-17 17:41:17 +0100

    add auth guard

--- listen-interface/src/routes/__root.tsx
+++ listen-interface/src/routes/__root.tsx
@@ -1,12 +1,28 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { GettingStarted } from "../components/GettingStarted";
 import { Layout } from "../components/Layout";
+import { useIsAuthenticated } from "../hooks/useIsAuthenticated";
 
 export const Route = createRootRoute({
-  component: () => (
-    <Layout>
-      <Outlet />
-      {import.meta.env.DEV && <TanStackRouterDevtools />}
-    </Layout>
-  ),
+  component: RootComponent,
+  beforeLoad: async ({ location }) => {
+    // Skip auth check for the root path since it redirects to /chat
+    if (location.pathname === "/") {
+      return;
+    }
+  },
 });
+
+function RootComponent() {
+  const { isAuthenticated, ready } = useIsAuthenticated();
+
+  if (!ready) {
+    return (
+      <Layout>
+        <></>
+      </Layout>
+    );
+  }
+
+  return <Layout>{isAuthenticated ? <Outlet /> : <GettingStarted />}</Layout>;
+}
