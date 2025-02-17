c2b72dccf79a8bc5f45cd17ace80efe11ff1e3ab
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-02-17 16:36:03 +0100

    remove merged preview, bump compose

--- preview/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PriceUpdates from "@/components/PriceUpdates";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen pt-16">
-      <PriceUpdates />
-    </div>
-  );
-}
