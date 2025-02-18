7b2dbe0ba8d8653c4c43a131ca6ae22a01386605
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-02-18 21:29:00 +0100

    more cors

--- listen-engine/src/server/mod.rs
+++ listen-engine/src/server/mod.rs
@@ -85,9 +85,8 @@ pub async fn run() -> std::io::Result<()> {
                     .supports_credentials()
                     .expose_headers(["content-type", "authorization"])
                     .max_age(3600)
-                    // Ensure specific headers are handled correctly
-                    .allowed_header(http::header::CONTENT_TYPE)
-                    .allowed_header(http::header::AUTHORIZATION),
+                    .allowed_header(actix_web::http::header::CONTENT_TYPE)
+                    .allowed_header(actix_web::http::header::AUTHORIZATION),
             )
             .wrap(middleware::Logger::default())
             .route("/healthz", web::get().to(healthz))
