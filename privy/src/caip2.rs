814ced38e6f7d8efe06101864b2787b86dd863a7
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-02-19 15:00:20 +0100

    parametrize caip2

--- privy/src/caip2.rs
+++ privy/src/caip2.rs
@@ -2,5 +2,35 @@ pub struct Caip2;
 
 impl Caip2 {
     pub const SOLANA: &str = "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp";
+
+    pub const ETHEREUM: &str = "eip155:1";
+    pub const BSC: &str = "eip155:56";
     pub const ARBITRUM: &str = "eip155:42161";
+    pub const BASE: &str = "eip155:8453";
+    pub const BLAST: &str = "eip155:81457";
+    pub const AVALANCHE: &str = "eip155:43114";
+    pub const POLYGON: &str = "eip155:137";
+    pub const SCROLL: &str = "eip155:534352";
+    pub const OPTIMISM: &str = "eip155:10";
+    pub const LINEA: &str = "eip155:59144";
+    pub const GNOSIS: &str = "eip155:100";
+    pub const FANTOM: &str = "eip155:250";
+    pub const MOONRIVER: &str = "eip155:1285";
+    pub const MOONBEAM: &str = "eip155:1284";
+    pub const BOBA: &str = "eip155:288";
+    pub const MODE: &str = "eip155:34443";
+    pub const METIS: &str = "eip155:1088";
+    pub const LISK: &str = "eip155:1135";
+    pub const AURORA: &str = "eip155:1313161554";
+    pub const SEI: &str = "eip155:1329";
+    pub const IMMUTABLE: &str = "eip155:13371";
+    pub const GRAVITY: &str = "eip155:1625";
+    pub const TAIKO: &str = "eip155:167000";
+    pub const CRONOS: &str = "eip155:25";
+    pub const FRAXTAL: &str = "eip155:252";
+    pub const ABSTRACT: &str = "eip155:2741";
+    pub const CELO: &str = "eip155:42220";
+    pub const WORLD: &str = "eip155:480";
+    pub const MANTLE: &str = "eip155:5000";
+    pub const BERACHAIN: &str = "eip155:80094";
 }
