2b67c63424cd9903b46a725e184efbc2ca2e70ac
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-02-28 18:43:30 +0100

    migrate to listen-tracing

--- listen-adapter/bin/adapter.rs
+++ listen-adapter/bin/adapter.rs
@@ -1,8 +1,8 @@
 use actix_cors::Cors;
 use actix_web::{middleware::Logger, web, App, HttpServer};
 use dotenv::dotenv;
+use listen_tracing::setup_tracing;
 use tracing::info;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use listen_adapter::{
     db::make_db,
@@ -17,20 +17,7 @@ use listen_adapter::{
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let is_systemd = std::env::var("IS_SYSTEMD_SERVICE").is_ok();
-
-    // Configure logging based on environment
-    if is_systemd {
-        // Use systemd formatting when running as a service
-        let journald_layer = tracing_journald::layer().expect("Failed to create journald layer");
-        tracing_subscriber::registry().with(journald_layer).init();
-    } else {
-        // Use standard formatting for non-systemd environments
-        tracing_subscriber::fmt()
-            .with_ansi(true)
-            .with_target(true)
-            .init();
-    }
+    setup_tracing();
 
     let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set");
     let host = std::env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
