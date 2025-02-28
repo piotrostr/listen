3279bc7de3a8e761418c7c52fc0657e33cd8c758
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-02-28 18:38:48 +0100

    use crate

--- listen-engine/bin/engine.rs
+++ listen-engine/bin/engine.rs
@@ -1,26 +1,8 @@
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use listen_tracing::setup_tracing;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Replace the basic init with a custom configuration
-    let is_systemd = std::env::var("IS_SYSTEMD_SERVICE").is_ok();
-
-    if is_systemd {
-        // Use systemd formatting when running as a service
-        let journald_layer = tracing_journald::layer().unwrap();
-        tracing_subscriber::registry().with(journald_layer).init();
-    } else {
-        // Use standard formatting for non-systemd environments
-        tracing_subscriber::fmt()
-            .with_ansi(true)
-            .with_target(true)
-            .init();
-    }
-
-    if !is_systemd {
-        dotenv::dotenv().ok();
-    }
-    listen_engine::metrics::init_metrics();
+    setup_tracing();
 
     tracing::info!("Starting listen-engine...");
 
