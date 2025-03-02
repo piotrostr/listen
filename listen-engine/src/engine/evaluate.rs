b7035c431b9d8a644f87432f53293c3e05209575
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-02 16:08:07 +0100

    nit

--- listen-engine/src/engine/evaluate.rs
+++ listen-engine/src/engine/evaluate.rs
@@ -396,7 +396,7 @@ impl Engine {
         self.save_pipeline(pipeline, &mut pipeline_hash).await?;
 
         self.process_all_steps(pipeline, &price_cache, &mut pipeline_hash)
-            .await;
+            .await?;
         self.save_pipeline(pipeline, &mut pipeline_hash).await?;
 
         let pipeline_done = self.collect_step_results(pipeline);
