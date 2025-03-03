b45ff2e18ca367c71ff73bc33a914a0c345583d3
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-03 20:01:27 +0100

    log message out

--- listen-kit/src/http/routes.rs
+++ listen-kit/src/http/routes.rs
@@ -145,7 +145,7 @@ async fn stream(
     let prompt = request.prompt.clone();
     let messages = request.chat_history.clone();
     println!("prompt: {}", prompt);
-    println!("messages: {:?}", messages);
+    println!("messages: {:#?}", messages);
 
     let signer: Arc<dyn TransactionSigner> =
         Arc::new(PrivySigner::new(state.privy.clone(), user_session.clone()));
