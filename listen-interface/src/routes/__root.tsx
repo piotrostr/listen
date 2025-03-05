48f90c66443a72317168e33f06ec00dcf7f7647c
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-05 20:50:32 +0100

    rollback

--- listen-interface/src/routes/__root.tsx
+++ listen-interface/src/routes/__root.tsx
@@ -1,9 +1,7 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { useEffect } from "react";
 import { GettingStarted } from "../components/GettingStarted";
 import { Layout } from "../components/Layout";
 import { useIsAuthenticated } from "../hooks/useIsAuthenticated";
-import { checkAppVersion } from "../utils/version";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -16,12 +14,6 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   const { isAuthenticated, ready } = useIsAuthenticated();
-
-  // Check for new version on component mount
-  useEffect(() => {
-    // This will automatically reload the page if a new version is detected
-    checkAppVersion();
-  }, []);
 
   if (!ready) {
     return (
