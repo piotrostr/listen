3a923fe8790bf25876ae00c73ccd86f591da84b5
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-05 21:14:59 +0100

    bump version

--- listen-interface/src/components/Layout.tsx
+++ listen-interface/src/components/Layout.tsx
@@ -201,7 +201,7 @@ export function VersionDisplay({ isSidebarOpen }: { isSidebarOpen: boolean }) {
         isSidebarOpen ? "px-4" : "justify-center"
       }`}
     >
-      {isSidebarOpen && <span>version: 1.1.3</span>}
+      {isSidebarOpen && <span>version: 1.1.4</span>}
     </div>
   );
 }
