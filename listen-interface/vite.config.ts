5dd987bad947f2776ba7e8a3181a4aefaee1420e
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-05 20:55:20 +0100

    nit

--- listen-interface/vite.config.ts
+++ listen-interface/vite.config.ts
@@ -37,6 +37,9 @@ export default defineConfig({
           },
         ],
       },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+      },
     }),
     visualizer(),
     compression(),
