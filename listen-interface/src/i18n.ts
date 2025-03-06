b0d539f210eabe0af33978c1a3d62a34631f7777
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-06 14:54:28 +0100

    nit

--- listen-interface/src/i18n.ts
+++ listen-interface/src/i18n.ts
@@ -4,11 +4,10 @@ import { initReactI18next } from "react-i18next";
 const resources = {
   en: {
     translation: {
-      hello: "Hello, world!",
       getting_started: {
         how_it_works: "How it works",
         how_it_works_description:
-          "listen is your single stop for on-chain trading with natural language",
+          "Listen is your single stop for on-chain trading with natural language",
         step_1: "1. create an account, you can use your email or wallet",
         step_2:
           "2. initialize a wallet for your AI agent, deposit funds and delegate access",
@@ -17,17 +16,16 @@ const resources = {
         questions:
           "Should you have any questions - ask the agent directly - Listen understands the tools it has access to and has a view of the portfolio its managing",
         warning:
-          "WARNING: listen is in early beta, things might not work as expected, use at own risk",
+          "WARNING: Listen is in early beta, things might not work as expected, use at own risk",
       },
     },
   },
   zh: {
     translation: {
-      hello: "你好，世界！",
       getting_started: {
         how_it_works: "如何运作",
         how_it_works_description:
-          "listen 是您的单一目的地，用于使用自然语言进行链上交易",
+          "Listen 是您的单一目的地，用于使用自然语言进行链上交易",
         step_1: "1. 创建一个帐户，您可以使用电子邮件或钱包",
         step_2: "2. 为您的 AI 代理初始化一个钱包，存入资金并委托访问",
         step_3: "3. 尽情享受吧！",
@@ -36,7 +34,7 @@ const resources = {
         questions:
           "如果您有任何问题 - 直接询问代理 - Listen 了解它拥有的工具并拥有其管理的投资组合的视图",
         warning:
-          "警告：listen 处于早期测试阶段，可能会出现意想不到的问题，请自行承担风险",
+          "警告：Listen 处于早期测试阶段，可能会出现意想不到的问题，请自行承担风险",
       },
     },
   },
