497f367de4587ecbf10a16c8b77b15159096be47
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-08 00:46:19 +0100

    rm out of date docs link

--- listen-interface/src/components/Layout.tsx
+++ listen-interface/src/components/Layout.tsx
@@ -109,15 +109,16 @@ export const SidebarContext = createContext<(open: boolean) => void>(() => {});
 
 function getBottomItems(t: (key: string) => string) {
   return [
-    {
-      href: "https://docs.listen-rs.com",
-      icon: () => (
-        <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z" />
-        </svg>
-      ),
-      label: t("layout.documentation"),
-    },
+    // TODO update docs when ready
+    // {
+    //   href: "https://docs.listen-rs.com",
+    //   icon: () => (
+    //     <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+    //       <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z" />
+    //     </svg>
+    //   ),
+    //   label: t("layout.documentation"),
+    // },
     {
       href: "https://github.com/piotrostr/listen",
       icon: () => (
