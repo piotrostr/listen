3fbba353030b10e3b12ea2d65e1afd000e994ad5
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-07 14:49:23 +0100

    nit

--- listen-interface/src/components/TopTokensDisplay.tsx
+++ listen-interface/src/components/TopTokensDisplay.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import { FaCheck } from "react-icons/fa6";
-import { IoBarChart } from "react-icons/io5";
 import { z } from "zod";
 import { useModal } from "../contexts/ModalContext";
 import { Socials } from "./Socials";
@@ -36,7 +34,6 @@ const formatNumber = (num: number) => {
 
 const TokenTile = ({ token }: { token: TopToken }) => {
   const [metadata, setMetadata] = useState<any>(null);
-  const [copied, setCopied] = useState(false);
   const { openChart } = useModal();
 
   useEffect(() => {
@@ -48,12 +45,6 @@ const TokenTile = ({ token }: { token: TopToken }) => {
       .then(setMetadata)
       .catch(console.error);
   }, [token.pubkey]);
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(token.pubkey);
-    setCopied(true);
-    setTimeout(() => setCopied(false), 1000);
-  };
 
   return (
     <div className="rounded-lg p-3 border border-blue-500/20 hover:border-blue-500/40 transition-colors">
@@ -85,31 +76,6 @@ const TokenTile = ({ token }: { token: TopToken }) => {
               {metadata?.mpl?.symbol || token.name}
             </a>
             <Socials tokenMetadata={metadata} pubkey={token.pubkey} />
-            <button
-              onClick={handleCopy}
-              className="hover:text-blue-400 hidden lg:block"
-            >
-              {copied ? (
-                <FaCheck size={12} />
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-4 h-4"
-                >
-                  <path d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
-                </svg>
-              )}
-            </button>
-            <button
-              onClick={() => openChart(token.pubkey)}
-              className="hover:text-blue-400 hidden lg:block"
-            >
-              <IoBarChart size={14} />
-            </button>
           </div>
           <div className="text-sm text-gray-500">
             ${token.price.toFixed(token.price < 0.01 ? 4 : 2)}
