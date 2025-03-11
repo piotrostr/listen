351270368ec1e19d95dac28fb6775d604c50202f
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-11 21:17:58 +0100

    nit

--- listen-interface/src/components/Layout.tsx
+++ listen-interface/src/components/Layout.tsx
@@ -340,12 +340,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
           )}
         </div>
       </div>
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-      </head>
     </SidebarContext.Provider>
   );
 }
