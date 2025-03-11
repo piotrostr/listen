69a25b672671b46abf26f7f2c47f79d6cc6b40ae
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-11 15:10:57 +0100

    rm scraper

--- listen-scraper/twitterapi/util.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-// Create a generic API response schema
-export const createApiResponseSchema = <T extends z.ZodTypeAny>(schema: T) =>
-  z.object({
-    data: schema,
-    status: z.enum(["success", "error"]),
-    msg: z.string(),
-  });
