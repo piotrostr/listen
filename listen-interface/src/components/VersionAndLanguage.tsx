0a270371201620a6d0dce8008f6fd54ed30a9478
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-16 18:52:38 +0100

    bump version

--- listen-interface/src/components/VersionAndLanguage.tsx
+++ listen-interface/src/components/VersionAndLanguage.tsx
@@ -7,7 +7,7 @@ export function VersionAndLanguageDisplay() {
   return (
     <div className="flex justify-around items-center w-full">
       <span className="text-xs text-gray-400">
-        {t("layout.version")}: 2.2.2
+        {t("layout.version")}: 2.2.3
       </span>
       <LanguageSwitcher />
     </div>
