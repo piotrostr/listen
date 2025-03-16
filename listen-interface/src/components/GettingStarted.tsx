458c88ad5888fb9d78e095b5498f58d0d3130bb6
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-16 21:40:08 +0100

    set base for getting started

--- listen-interface/src/components/GettingStarted.tsx
+++ listen-interface/src/components/GettingStarted.tsx
@@ -1,41 +1,71 @@
-import { usePrivy } from "@privy-io/react-auth";
 import { useTranslation } from "react-i18next";
-import { FaExclamationTriangle } from "react-icons/fa";
+import { BetaWarning } from "./BetaWarning";
+
+const OutlineButton = ({ children }: { children: React.ReactNode }) => {
+  return <button>{children}</button>;
+};
+
+const GradientOutlineButton = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative">
+      {children}
+      <div className="absolute inset-0">
+        <svg
+          width="358"
+          height="70"
+          viewBox="0 0 358 70"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            width="358"
+            height="70"
+            rx="16"
+            fill="url(#paint0_linear_2033_12189)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_2033_12189"
+              x1="1.20047e-05"
+              y1="40"
+              x2="358"
+              y2="35"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#FD98A2" />
+              <stop offset="0.315" stop-color="#FB2671" />
+              <stop offset="0.675" stop-color="#A42CCD" />
+              <stop offset="1" stop-color="#7F4AFB" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  );
+};
 
 export function GettingStarted() {
-  const { login, ready } = usePrivy();
   const { t } = useTranslation();
 
   return (
     <div className="flex flex-col items-center gap-4 p-2 w-full overflow-hidden">
-      {/* Getting Started Section */}
       <div className="w-full max-w-2xl mx-auto flex flex-col items-center text-center gap-2">
         <h2 className="text-xl lg:text-2xl font-bold mt-5 mb-2">
-          {t("getting_started.how_it_works")}
+          {t("getting_started.listen_hi")}
         </h2>
         <p className="text-sm lg:text-base">
-          {t("getting_started.how_it_works_description")}
+          {t("getting_started.listen_intro")}
         </p>
-        <p className="text-sm lg:text-base">{t("getting_started.step_1")}</p>
-        <p className="text-sm lg:text-base">{t("getting_started.step_2")}</p>
-        <p className="text-sm lg:text-base">{t("getting_started.step_3")}</p>
-        <br />
-        <button
-          onClick={login}
-          disabled={!ready}
-          className="p-2 border-2 border-[#2D2D2D] rounded-lg bg-black/40 backdrop-blur-sm flex items-center px-3 text-sm hover:bg-[#2D2D2D]"
-        >
-          {t("getting_started.get_started")}
-        </button>
-        <p className="text-sm max-w-md text-center mt-3">
-          {t("getting_started.questions")}
+        <p className="text-sm lg:text-base">
+          {t("getting_started.where_should_we_start")}
         </p>
       </div>
-
-      <span className="text-sm my-8 mb-1 flex items-center gap-2 flex-col lg:flex-row text-center">
-        <FaExclamationTriangle className="text-yellow-500" />
-        {t("getting_started.warning")}
-      </span>
+      <GradientOutlineButton>
+        <p className="text-sm lg:text-base text-white">
+          {t("getting_started.lets_make_a_trade")}
+        </p>
+      </GradientOutlineButton>
+      <BetaWarning />
     </div>
   );
 }
