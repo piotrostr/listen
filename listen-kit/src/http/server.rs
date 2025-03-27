35ca1119a3038bcf315dd83d13b5be9eda034889
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-03-27 23:58:30 +0100

    try fix cors err

--- listen-kit/src/http/server.rs
+++ listen-kit/src/http/server.rs
@@ -17,12 +17,18 @@ pub async fn run_server(
         App::new()
             .wrap(Logger::default())
             .wrap(Compress::default())
-            .wrap(Cors::permissive())
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allow_any_method()
+                    .allow_any_header()
+                    .max_age(3600),
+            )
             .app_data(state.clone())
             .service(healthz)
             .service(stream)
             .service(auth)
-        // .service(suggest)
+            .service(suggest)
     })
     .bind("0.0.0.0:6969")?
     .run()
