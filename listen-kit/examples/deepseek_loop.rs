71821518290053a7dc73f0bc541cab0214ad3b6c
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-04-03 18:35:01 +0200

    nit

--- listen-kit/examples/deepseek_loop.rs
+++ listen-kit/examples/deepseek_loop.rs
@@ -12,24 +12,16 @@ async fn main() -> Result<()> {
     let reasoning_loop = ReasoningLoop::new(Model::DeepSeek(Arc::new(
         create_listen_agent_deepseek("en".to_string()),
     )))
-    .with_stdout(false);
+    .with_stdout(true);
 
     let signer = LocalSolanaSigner::new(env("SOLANA_PRIVATE_KEY"));
-
-    let (tx, mut rx) = tokio::sync::mpsc::channel(1024);
-
-    tokio::spawn(async move {
-        while let Some(response) = rx.recv().await {
-            print!("{:?}\n", response);
-        }
-    });
 
     let result = spawn_with_signer(Arc::new(signer), || async move {
         reasoning_loop
             .stream(
                 "what can you tell me about listen?".to_string(),
                 vec![],
-                Some(tx),
+                None,
             )
             .await
     })
