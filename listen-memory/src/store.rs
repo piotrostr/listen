0a4142340ca88ff1816ee7efdd10b8eebcef19be
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-04-09 15:01:59 +0200

    parametrize

--- listen-memory/src/store.rs
+++ listen-memory/src/store.rs
@@ -1,33 +1,38 @@
 use anyhow::Result;
 use listen_mongo::MongoClient;
 
-use crate::memory_note::MemoryNote;
+use crate::{memory_note::MemoryNote, util::must_get_env};
 pub struct MemoryStore {
     client: MongoClient,
+    collection_name: String,
 }
-
-const COLLECTION_NAME: &str = "memories";
 
 impl MemoryStore {
     pub async fn from_env() -> Result<Self> {
         let client = MongoClient::from_env().await?;
-        Ok(Self { client })
+        let collection_name = must_get_env("MONGODB_COLLECTION_NAME");
+        Ok(Self {
+            client,
+            collection_name,
+        })
     }
 
     pub async fn update_memory(&self, id: &str, memory: MemoryNote) -> Result<()> {
-        self.client.update(COLLECTION_NAME, id, memory).await?;
+        self.client
+            .update(&self.collection_name, id, memory)
+            .await?;
         Ok(())
     }
 
     pub async fn add_memory(&self, memory: MemoryNote) -> Result<()> {
         self.client
-            .insert_one_with_id(COLLECTION_NAME, &memory.id.to_string(), memory)
+            .insert_one_with_id(&self.collection_name, &memory.id.to_string(), memory)
             .await?;
         Ok(())
     }
 
     pub async fn get_memory(&self, id: &str) -> Result<Option<MemoryNote>> {
-        let memory = self.client.find_by_id(COLLECTION_NAME, id).await?;
+        let memory = self.client.find_by_id(&self.collection_name, id).await?;
         Ok(memory)
     }
 }
