03b0eaaef7e382d92989aa82a702f7c09fc08e3f
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-04-26 00:06:55 +0200

    nit

---
+++ listen-mem0/ts/scripts/test-req.ts
@@ -0,0 +1,13 @@
+const res = await fetch("http://localhost:6969/memories/search", {
+  method: "POST",
+  body: JSON.stringify({
+    query: "what's my name",
+    filters: {
+      user_id: "some-user",
+    },
+  }),
+});
+
+console.log(res.status);
+
+console.log(await res.json());
