51905b73220a66545ae80e948ac5427ff0cc61d8
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-05-02 15:00:40 +0200

    add 'feeling lucky' prompt to the carousel

--- listen-interface/src/i18n/translations/recommended-questions.ts
+++ listen-interface/src/i18n/translations/recommended-questions.ts
@@ -1,5 +1,8 @@
 export const recommendedQuestions = {
   en: {
+    im_feeling_lucky_question:
+      "Please check the tokens with most volume in the last 15 minutes between the 100-500k market cap. Find the ones that are currently retracing, we are looking for the extension between fib 0.618 and 0.782 to bid",
+    im_feeling_lucky_display: "I'm feeling lucky",
     what_actions_can_you_perform_for_me:
       "What actions can you perform for me? Why would I use an AI agent to trade?",
     how_do_pipelines_work_and_what_pipelines_can_you_create_for_me:
@@ -21,6 +24,9 @@ export const recommendedQuestions = {
     research_arcdotfun_for_me: "Research $arc for me",
   },
   zh: {
+    im_feeling_lucky_question:
+      "请检查过去 15 分钟内 100-500k 市值之间交易量最大的代币。找到当前正在回调的代币，我们正在寻找在斐波那契 0.618 和 0.782 之间的延伸，以进行一些购买",
+    im_feeling_lucky_display: "我很幸运",
     what_actions_can_you_perform_for_me: "你能执行哪些操作？",
     how_do_pipelines_work_and_what_pipelines_can_you_create_for_me:
       "自动化任务如何运作，你能为我创建哪些自动化任务？",
