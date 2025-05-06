8a628537a038b20bfd8c4675a06d72e7da86a51f
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-05-06 12:48:36 +0200

    add err component

--- listen-interface/src/routes/__root.tsx
+++ listen-interface/src/routes/__root.tsx
@@ -5,6 +5,23 @@ import { ChatProvider } from "../contexts/ChatContext";
 import { ModalProvider } from "../contexts/ModalContext";
 import { PanelProvider } from "../contexts/PanelContext";
 
+function ErrorComponent() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white">
+      <div className="text-center p-8 rounded-lg">
+        <h1 className="text-2xl font-bold mb-4">Oops! Something went wrong</h1>
+        <p className="mb-4">We encountered an error while loading this page.</p>
+        <a
+          href="/"
+          className="px-4 py-2 bg-[#2D2D2D] hover:bg-[#3D3D3D] rounded-lg transition-colors"
+        >
+          Return Home
+        </a>
+      </div>
+    </div>
+  );
+}
+
 export const Route = createRootRoute({
   component: () => (
     <PanelProvider>
@@ -18,4 +35,5 @@ export const Route = createRootRoute({
       </ChatProvider>
     </PanelProvider>
   ),
+  errorComponent: ErrorComponent,
 });
