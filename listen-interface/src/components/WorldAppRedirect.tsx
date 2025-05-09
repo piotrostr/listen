97acf7d26216e6b1deba4cd715efc80dd6569cd1
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-05-09 17:15:58 +0200

    redirect on click anywhere on the worldappredirect

--- listen-interface/src/components/WorldAppRedirect.tsx
+++ listen-interface/src/components/WorldAppRedirect.tsx
@@ -39,29 +39,29 @@ const RedirectArrow = () => (
 
 export function WorldAppRedirect({ app }: { app: WorldMiniApp }) {
   return (
-    <Container>
-      <div className="flex flex-row py-4 px-4 items-center w-full justify-between">
-        <div className="flex flex-row items-center">
-          <AppLogo src={app.logo_img_url} alt={app.name} />
-          <div className="flex flex-col p-2">
-            <div className="flex flex-row items-center space-x-2">
-              <div className="font-space-grotesk font-normal text-2xl leading-8 tracking-[-0.03em] text-center align-middle">
-                {app.name}
+    <a
+      href={`worldapp://mini-app?app_id=${app.app_id}`}
+      title="Open in World App"
+      className="cursor-pointer"
+    >
+      <Container>
+        <div className="flex flex-row py-4 px-4 items-center w-full justify-between">
+          <div className="flex flex-row items-center">
+            <AppLogo src={app.logo_img_url} alt={app.name} />
+            <div className="flex flex-col p-2">
+              <div className="flex flex-row items-center space-x-2">
+                <div className="font-space-grotesk font-normal text-2xl leading-8 tracking-[-0.03em] text-center align-middle">
+                  {app.name}
+                </div>
+              </div>
+              <div className="font-dm-sans font-light text-[14px] leading-[16px] tracking-[0%] align-middle text-[#868686]">
+                {app.world_app_description}
               </div>
             </div>
-            <div className="font-dm-sans font-light text-[14px] leading-[16px] tracking-[0%] align-middle text-[#868686]">
-              {app.world_app_description}
-            </div>
           </div>
-        </div>
-        <a
-          href={`worldapp://mini-app?app_id=${app.app_id}`}
-          className="p-2 hover:bg-[#1e1e21] rounded-full transition-colors"
-          title="Open in World App"
-        >
           <RedirectArrow />
-        </a>
-      </div>
-    </Container>
+        </div>
+      </Container>{" "}
+    </a>
   );
 }
