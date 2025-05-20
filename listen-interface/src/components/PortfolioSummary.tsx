861754f9ccfd995220aca9c2d75b7e78d5db1e94
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-05-20 13:23:46 +0200

    nit

--- listen-interface/src/components/PortfolioSummary.tsx
+++ listen-interface/src/components/PortfolioSummary.tsx
@@ -128,15 +128,15 @@ export const CopyIcon = () => (
     <path
       d="M7.33333 14.6667C6.59695 14.6667 6 14.0698 6 13.3334V7.3343C6 6.59756 6.59753 6.00046 7.33427 6.00098L13.3343 6.00521C14.0703 6.00573 14.6667 6.60253 14.6667 7.33857V13.3334C14.6667 14.0698 14.0697 14.6667 13.3333 14.6667H7.33333Z"
       stroke="#D9D9D9"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M11.3335 5.61901V2.67268C11.3335 1.93666 10.7371 1.33986 10.0011 1.33934L2.66776 1.33423C1.93102 1.33372 1.3335 1.93082 1.3335 2.66756V9.99996C1.3335 10.7364 1.93045 11.3333 2.66683 11.3333H5.61921"
       stroke="#D9D9D9"
-      stroke-width="1.5"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinejoin="round"
     />
   </svg>
 );
