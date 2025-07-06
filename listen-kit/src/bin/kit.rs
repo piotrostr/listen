6c8905188b134ea205c3c8b40f2fef82696c9c01
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-07-06 20:38:35 +0200

    make mongo/neo4j opt-in configurable with env

--- listen-kit/src/bin/kit.rs
+++ listen-kit/src/bin/kit.rs
@@ -4,30 +4,35 @@ use {
     listen_memory::graph::GraphMemory,
     listen_mongo::MongoClient,
     privy::{config::PrivyConfig, Privy},
+    std::sync::Arc,
 };
 
 #[cfg(feature = "http")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Initialize Privy client
-
-    use std::sync::Arc;
     let privy_client =
         Privy::new(PrivyConfig::from_env().map_err(|e| {
             std::io::Error::new(std::io::ErrorKind::Other, e)
         })?);
 
-    let mongo = MongoClient::from_env().await.map(Arc::new).ok();
+    let mongo = if std::env::var("WITH_MONGO").is_ok() {
+        MongoClient::from_env().await.map(Arc::new).ok()
+    } else {
+        None
+    };
     if mongo.is_none() {
         tracing::warn!("Starting without MongoDB, chats will not be saved");
-    }
+    };
 
-    let global_memory = GraphMemory::from_env().await.map(Arc::new).ok();
+    let global_memory = if std::env::var("WITH_NEO4J").is_ok() {
+        GraphMemory::from_env().await.map(Arc::new).ok()
+    } else {
+        None
+    };
+
     if global_memory.is_none() {
-        tracing::warn!(
-            "Starting without global memory, graph memory will not be used"
-        );
-    }
+        tracing::warn!("Starting without global memory");
+    };
 
     #[cfg(feature = "engine")]
     {
