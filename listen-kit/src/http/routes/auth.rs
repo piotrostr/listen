2542e8e22d680389d9a3d69e3aadbd347581ef9a
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-07-08 01:16:09 +0200

    more verbosity

--- listen-kit/src/http/routes/auth.rs
+++ listen-kit/src/http/routes/auth.rs
@@ -1,10 +1,21 @@
-use crate::http::{middleware::verify_auth, state::AppState};
+use crate::http::{
+    middleware::{verify_auth, verify_token},
+    state::AppState,
+};
 use actix_web::{get, web, Error, HttpRequest, HttpResponse};
 use anyhow::Result;
 use serde_json::json;
 
 #[get("/auth")]
 async fn auth(req: HttpRequest) -> Result<HttpResponse, Error> {
+    let claims = match verify_token(&req).await {
+        Ok(claims) => claims,
+        Err(e) => {
+            return Ok(HttpResponse::Unauthorized()
+                .json(json!({ "error": e.to_string() })))
+        }
+    };
+
     let user_session = match verify_auth(&req).await {
         Ok(session) => session,
         Err(e) => {
@@ -26,5 +37,6 @@ async fn auth(req: HttpRequest) -> Result<HttpResponse, Error> {
         "wallet_address": user_session.wallet_address,
         "user_id": user_session.user_id,
         "privy_app_id": state.privy.config.app_id,
+        "claims": claims,
     })))
 }
