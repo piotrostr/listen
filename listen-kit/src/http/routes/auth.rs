2f1db40f6f319765bedb47d05a3afec26f3f5ef6
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2025-07-07 17:57:48 +0200

    more verbosity

--- listen-kit/src/http/routes/auth.rs
+++ listen-kit/src/http/routes/auth.rs
@@ -1,5 +1,5 @@
-use crate::http::middleware::verify_auth;
-use actix_web::{get, Error, HttpRequest, HttpResponse};
+use crate::http::{middleware::verify_auth, state::AppState};
+use actix_web::{get, web, Error, HttpRequest, HttpResponse};
 use anyhow::Result;
 use serde_json::json;
 
@@ -13,8 +13,18 @@ async fn auth(req: HttpRequest) -> Result<HttpResponse, Error> {
         }
     };
 
+    let state = match req.app_data::<web::Data<AppState>>() {
+        Some(state) => state,
+        None => {
+            return Ok(HttpResponse::InternalServerError()
+                .json(json!({ "error": "App state not found" })))
+        }
+    };
+
     Ok(HttpResponse::Ok().json(json!({
         "status": "ok",
         "wallet_address": user_session.wallet_address,
+        "user_id": user_session.user_id,
+        "privy_app_id": state.privy.config.app_id,
     })))
 }
